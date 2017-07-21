c3fd72f6ee61db0836ac1086ea256c92532b4d34
Martin <mac.cat@freenet.de>
2017-07-21 21:13:00 +0200

    added whatsapp project

---
+++ SteamVR-WhatsApp/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SteamVR_WebKit;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+
+namespace SteamVR_WhatsApp
+{
+    class Program
+    {
+        static WebKitOverlay overlay;
+
+        static string script;
+
+        static void Main(string[] args)
+        {
+            SteamVR_WebKit.SteamVR_WebKit.Init(new CefSharp.CefSettings() { PersistSessionCookies = true, CachePath = "chrome-cache" });
+            SteamVR_WebKit.SteamVR_WebKit.FPS = 30;
+
+            script = File.ReadAllText("Resources/Script.js");
+
+            //SteamVR_WebKit.JsInterop.Notifications.RegisterIcon("default", new Bitmap(Environment.CurrentDirectory + "\\Resources\\spotify-logo-small.png")); //-- leads to exception
+
+            overlay = new WebKitOverlay(new Uri("https://web.whatsapp.com/"), 1024, 1024, "whatsapp", "WhatsApp", OverlayType.Dashboard);
+            overlay.CachePath = "chrome-cache";
+            overlay.DashboardOverlay.Width = 2.0f;
+            overlay.DashboardOverlay.SetThumbnail("Resources/whatsapp-icon.png");
+            overlay.BrowserPreInit += Overlay_BrowserPreInit;
+            if (args.Length > 0 && args[0] == "--debug")
+            {
+                overlay.BrowserReady += Overlay_BrowserReady;
+            }
+            overlay.StartBrowser();
+
+            EventHandler<CefSharp.LoadingStateChangedEventArgs> handler = PageReady;
+            overlay.Browser.LoadingStateChanged += handler;
+
+            SteamVR_Application application = new SteamVR_Application();
+            application.InstallManifest(true);
+            if (args.Length > 0 && args[0] == "--debug")
+            {
+                application.RemoveManifest();
+            }
+
+            SteamVR_WebKit.SteamVR_WebKit.RunOverlays(); // Runs update/draw calls for all active overlays. And yes, it's blocking.
+        }
+
+        private static void Browser_ConsoleMessage(object sender, CefSharp.ConsoleMessageEventArgs e)
+        {
+            string[] srcSplit = e.Source.Split('/'); // We only want the filename
+            //Console.WriteLine("[CONSOLE " + srcSplit[srcSplit.Length - 1] + ":" + e.Line + "] " + e.Message);
+        }
+
+        private static void Overlay_BrowserReady(object sender, EventArgs e)
+        {
+            overlay.Browser.GetBrowser().GetHost().ShowDevTools();
+        }
+
+        private static void Overlay_BrowserPreInit(object sender, EventArgs e)
+        {
+            Console.WriteLine("Browser is ready.");
+
+            overlay.Browser.ConsoleMessage += Browser_ConsoleMessage;
+            overlay.Browser.RegisterJsObject("notifications", new SteamVR_WebKit.JsInterop.Notifications(overlay.DashboardOverlay));
+        }
+
+        private static void PageReady(object sender, CefSharp.LoadingStateChangedEventArgs args)
+        {
+            if (!args.IsLoading)
+            {
+                overlay.Browser.GetBrowser().MainFrame.ExecuteJavaScriptAsync(script);
+                Uri uri = new Uri(Environment.CurrentDirectory + "/Resources/login_page.html");
+                string login_page_path = "file:///" + uri.AbsolutePath;
+                //string escapedFilepath = login_page_path.Replace("\\", "\\\\");
+                Console.WriteLine("Login page path: " + login_page_path);
+                //Thread.Sleep(5000);
+                overlay.Browser.GetBrowser().MainFrame.ExecuteJavaScriptAsync("onPageLoaded('" + login_page_path + "')");
+                //overlay.Browser.GetBrowser().MainFrame.ExecuteJavaScriptAsync("onPageLoaded('text.html');");
+                //overlay.TryExecAsyncJS(script);
+                //overlay.TryExecAsyncJS("onPageLoaded('file://" + Environment.CurrentDirectory + "/Resources/login_page.html')");
+            }
+        }
+    }
+}
---
+++ SteamVR-WhatsApp/Resources/script.js
@@ -0,0 +1,92 @@
+﻿var a;
+
+function onPageLoaded(login_page_path) {   
+    if (checkLoginPage()) {
+        console.log("we are not logged in");
+        // we are not logged in 
+        openLoginPopup(login_page_path); // TODO: somehow close it again!
+        
+    } else {
+        console.log("we are logged in");
+        // we are logged in
+        setTimeout(checkNewMessages, 1000); // check for new messages every second
+    }
+}
+
+// check if we are on login-page
+function checkLoginPage() {
+    var elements = document.getElementsByClassName("avatar-body");
+    return elements.length == 0;
+}
+
+// get qr code as base64 string
+function getQrCodeString() {
+    var imgs = document.querySelectorAll(".qrcode > img");
+    if (imgs.length == 0) {
+        return "";
+    }
+    return imgs[0].src;
+}
+
+// open new popup so user can scan on PC
+function openLoginPopup(page_path) {
+    var qrstring = getQrCodeString();
+    if (qrstring != "") {
+        console.log("opening popup");
+        console.log(page_path)
+        var win = window.open(page_path, "WhatsApp Login", "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, width=350, height=300");
+        console.log(win.document.body)
+        qrcode_div = win.document.getElementById("qrcode");
+        qrcode_div.innerHTML = "<img src='" + qrstring + "' style='display:block;'>"
+        //console.log(win.document.body);
+    } else {
+        setTimeout(function () {
+            console.log("didn't find qrcode, trying again in 1s..");
+            openLoginPopup(page_path)
+        }, 1000);
+    }
+
+}
+
+// check for new messages
+function checkNewMessages() {
+    console.log("checking for new messages");
+    var unread_msgs = document.getElementsByClassName("unread");
+    console.log("Found " + unread_msgs.length + " unread messages");
+    for (unread of unread_msgs) {
+        var title_div = unread.getElementsByClassName("chat-title");
+        if (title_div.length == 0) continue;
+
+        var title_span = title_div[0].getElementsByTagName("span");
+        if (title_span.length == 0) continue;
+
+        var title = title_span[0].title;
+
+        var last_msg_div = unread.getElementsByClassName("last-msg");
+        if (last_msg_div.length == 0) continue;
+        var message = last_msg_div[0].title;
+
+        var sender_span = last_msg_div[0].getElementsByClassName("sender");
+        var sender = "";
+        if (sender_span.length != 0) {
+            var text_span = last_msg_div[0].getElementsByTagName("span");
+            if (text_span.length != 0) {
+                sender = text_span[0].innerText;
+            }
+        }
+
+        messageNotification(title, sender, text);
+
+    }
+}
+
+// send a notification with sender and message text
+function messageNotification(title, sender, text) {
+    var message_string;
+    message_string = "title\n";
+    if (sender != "") {
+        message_string += sender + ": ";
+    }
+    message_string += text;
+    notifications.sendNotification(message_string);
+}
